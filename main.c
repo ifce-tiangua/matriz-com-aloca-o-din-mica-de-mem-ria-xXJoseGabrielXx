902e7e2eb341b7b6280ab0d00e99d23b8d5665be
xXJoseGabrielXx <132611883+xXJoseGabrielXx@users.noreply.github.com>
2024-11-17 12:33:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
+	int **matriz;
+	int linha, coluna;
+	int i, j;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+	scanf("%d", &linha);
+	scanf("%d", &coluna);
+
+     matriz = (int **)malloc(linha * sizeof(int *));
+    for (i = 0; i < linha; i++) {
+        matriz[i] = (int *)malloc(coluna * sizeof(int));
+    }
+
+
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("Matriz lida:\n");
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < linha; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
 
     return 0;
 }
