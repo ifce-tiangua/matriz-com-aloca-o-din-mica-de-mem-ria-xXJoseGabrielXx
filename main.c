901750f391b7537f08fb31ff0a1dbfb453b4e944
xXJoseGabrielXx <132611883+xXJoseGabrielXx@users.noreply.github.com>
2024-11-21 17:31:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int **matriz;
-    int linha, coluna;
-    int i, j;
-
-    scanf("%d", &linha);
-    scanf("%d", &coluna);
-
-    matriz = (int **)malloc(linha * sizeof(int *));
-    for (i = 0; i < linha; i++) {
-        matriz[i] = (int *)malloc(coluna * sizeof(int));
+void imprimir_matriz(int **mat, int colunas, int linhas)
+{
+    if (linhas == 0 || colunas == 0)
+    {
+        printf("[matriz vazia]");
+        return;
     }
-
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++) {
-            scanf("%d", &matriz[i][j]);
-        }
-    }
-
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++) {
-            printf("%d", matriz[i][j]);
-            if (j < coluna - 1) {
+    for (int l = 0; l < linhas; l++)
+    {
+        for (int c = 0; c < colunas; c++)
+        {
+            printf("%d", mat[l][c]);
+            if (c < colunas - 1)
+            {
                 printf(" ");
             }
         }
+
         printf("\n");
+    }
+}
 
+void destroi_matriz(int **mat, int linhas)
+{
+    for (int l = 0; l < linhas; l++)
+        free(mat[l]);
+    free(mat);
+}
+
+int main()
+{
+
+    int lin, col;
+
+    scanf("%d", &lin);
+    scanf("%d", &col);
+
+    int **matriz;
+
+    matriz = (int **)malloc(lin * sizeof(int *));
+    for (int i = 0; i < lin; i++)
+        matriz[i] = (int *)malloc(col * sizeof(int));
+
+    for (int l = 0; l < lin; l++)
+    {
+        for (int c = 0; c < col; c++)
+        {
+            scanf("%d", &matriz[l][c]);
+        }
     }
 
-    for (i = 0; i < linha; i++) {
-        free(matriz[i]);
-    }
-    free(matriz);
+    imprimir_matriz(matriz, col, lin);
+    destroi_matriz(matriz, lin);
 
     return 0;
 }
